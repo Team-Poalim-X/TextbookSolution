5271790e28413c0d615582f07e8f56e236c5b40f
Shaul Daon <daonsh@gmail.com>
2018-11-04 14:07:25 +0200

    fix jasmine tests to pre-create the user in neo4j

--- bankend/spec/AccountSpec.js
+++ bankend/spec/AccountSpec.js
@@ -1,21 +1,26 @@
 const Account = require('../account');
-/*
+
 describe("Account", function() {
+    beforeAll(async function() {
+        await Account.ensure_account_exists("TEST2-at-kashyoo.com");
+    });
+
     it("return null when username does not exist", async function() {
         var balance = await Account.get_balance_for_user("nonexist");
 	expect (balance).toEqual(null);
     });
 
-    it("return 100 for test user", async function() {
+    it("return 7000 for test user", async function() {
         var balance = await Account.get_balance_for_user("TEST2-at-kashyoo.com");
-	expect (balance).toEqual(100);
+	expect (balance).toEqual(7000);
     });
 });
-*/
 
+/*
 describe("Account", function() {
     it("always succeed", async function() {
     var balance = 5;
 	expect (balance).toEqual(5);
     });
 });
+*/
